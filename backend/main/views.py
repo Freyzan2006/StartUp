6e47426c71cbd31b6d8b5a8744238815d66eb8b1
freyzan <freyzan2006@gmail.com>
2024-11-27 19:10:44 +0500

    Сойденил ветку backend части

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hi(request):
+    if request.method == "GET":
+        return HttpResponse("<h1>Hello World</h1>")
+    else:
+        return HttpResponse("<h1>List Not Found</h1>")
