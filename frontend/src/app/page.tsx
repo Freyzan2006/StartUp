b36d74cceb3a4a59e9138162964be32f3a62b86b
RustamS <umoney1428@gmail.com>
2024-11-28 20:51:14 +0300

    create header,navbar,container file and scss.module file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+import Header from "@/components/layout/Header/Header";
+import Button from "@/components/UI/Button";
+import Container from "@/components/layout/Container/Container";
 import helloWorldService from "@/services/HelloWorld.service";
 import { useQuery } from "@tanstack/react-query";
 
@@ -12,7 +15,7 @@ interface IHelloWorld {
 
 export default function Home() {
  
-
+  
   // const { data, error, isLoading } = useQuery(['todo', id], () =>
   //   helloWorldService.getItem(id)
   // );
@@ -35,6 +38,9 @@ export default function Home() {
     <div>
       <h1>Data for item {data?.id}:</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Header />
+      <Button />
+      <Container />
     </div>
   );
 }
---
+++ frontend/src/components/layout/Container/Container.tsx
@@ -0,0 +1,9 @@
+import './Container.module.scss'
+
+const Container = () => {
+  return (
+    <div>Container</div>
+  )
+}
+
+export default Container
